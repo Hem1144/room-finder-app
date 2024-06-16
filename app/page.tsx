099c0792050f8058385a589d96202e7bf0a3d201
Hem1144 <hemlaldulal7@gmail.com>
2024-06-16 23:09:25 +0545

    feat: Reactor

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main >
-      <h1>Hello from dulal room-finder-apps</h1>
+      <h1>Hello we are from room-finder-apps</h1>
     </main>
   );
 }
